dedaed24d34a86f049153c2f602f9a1daecc849f
Jozsef Vass <joco.udv@gmail.com>
2024-05-14 22:31:51 +0100

    Add verifications table and generate TOTP code (#8)

--- db/schema.ts
+++ db/schema.ts
@@ -3,6 +3,7 @@ import { createId as cuid } from '@paralleldrive/cuid2';
 import { relations } from 'drizzle-orm';
 import {
 	index,
+	integer,
 	pgTable,
 	text,
 	timestamp,
@@ -137,3 +138,60 @@ export const connetionRelations = relations(connections, ({ one }) => ({
 		relationName: 'user_connections',
 	}),
 }));
+
+export const verifications = pgTable(
+	'verifications',
+	{
+		id: varchar('id', { length: 25 })
+			.primaryKey()
+			.notNull()
+			.$defaultFn(() => cuid()),
+		createdAt: timestamp('createdAt', { withTimezone: true })
+			.notNull()
+			.defaultNow(),
+		updatedAt: timestamp('updatedAt', { withTimezone: true })
+			.notNull()
+			.defaultNow()
+			.$onUpdateFn(() => new UTCDate()),
+		/**
+		 * The type of verification, e.g. "email", "phone" or "onboarding"
+		 */
+		type: text('type').notNull(),
+		/**
+		 * The thing we're trying to verify, e.g. a user's email or phone number
+		 */
+		target: text('target').notNull(),
+		/**
+		 * The secret key used to generate the otp
+		 */
+		secret: text('secret').notNull(),
+		/**
+		 * The algorithm used to generate the otp
+		 */
+		algorithm: text('algorithm').notNull(),
+		/**
+		 * The number of digits in the otp
+		 */
+		digits: integer('digits').notNull(),
+		/**
+		 * The number of seconds the otp is valid for
+		 */
+		period: integer('period').notNull(),
+		/**
+		 * The valid characters for the otp
+		 */
+		charSet: text('charSet').notNull(),
+		/**
+		 * When it's safe to delete this verification
+		 */
+		expiresAt: timestamp('expiresAt', { withTimezone: true }).notNull(),
+	},
+	table => {
+		return {
+			unique_target_type: unique('unique_target_type').on(
+				table.target,
+				table.type,
+			),
+		};
+	},
+);
