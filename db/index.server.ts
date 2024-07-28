62f51973142d71272b7398fb505faee663ef540f
Jozsef <joco.udv@gmail.com>
2024-07-28 12:49:59 +0100

    Remove logger

--- db/index.server.ts
+++ db/index.server.ts
@@ -1,22 +1,13 @@
 import { remember } from '@epic-web/remember';
 import { Pool, neonConfig } from '@neondatabase/serverless';
-import { type Logger } from 'drizzle-orm/logger';
 import { drizzle } from 'drizzle-orm/neon-serverless';
 import ws from 'ws';
 import * as schema from './schema';
 
-class MyLogger implements Logger {
-	logQuery(query: string, params: unknown[]): void {
-		// console.log('💥 ', { params });
-		console.log(params);
-		console.log(query);
-	}
-}
-
 export const db = remember('drizzle', () => {
 	neonConfig.webSocketConstructor = ws;
 	const pool = new Pool({ connectionString: process.env.DB_URL });
-	return drizzle(pool, { schema, logger: new MyLogger() });
+	return drizzle(pool, { schema });
 });
 
 export type Transaction = Parameters<Parameters<typeof db.transaction>[0]>[0];
