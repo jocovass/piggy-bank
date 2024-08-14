5972960684e342f5e00d25ecec1c8d96d763cbfc
Jozsef <joco.udv@gmail.com>
2024-08-14 22:38:22 +0100

    Add pie chart-

--- app/components/total-balance.tsx
+++ app/components/total-balance.tsx
@@ -1,7 +1,12 @@
 import { useMemo } from 'react';
-import { Line, LineChart } from 'recharts';
+import { Label, Pie, PieChart } from 'recharts';
 import { Card, CardContent } from '~/app/components/ui/card';
-import { type ChartConfig, ChartContainer } from '~/app/components/ui/chart';
+import {
+	type ChartConfig,
+	ChartContainer,
+	ChartTooltip,
+	ChartTooltipContent,
+} from '~/app/components/ui/chart';
 import { formatCurrency } from '~/app/utils/format-currency';
 import { type Account, type BankConnection } from '~/db/schema';
 
@@ -10,13 +15,6 @@ export type TotalBalanceProps = {
 		accounts: Account[];
 	})[];
 };
-
-const chartConfig = {
-	amount: {
-		label: 'Amount',
-		color: 'hsl(var(--chart-green))',
-	},
-} satisfies ChartConfig;
 
 export default function TotalBalance({ connections }: TotalBalanceProps) {
 	const totalBalance = useMemo(
@@ -33,34 +31,91 @@ export default function TotalBalance({ connections }: TotalBalanceProps) {
 		[connections],
 	);
 
+	const chartConfig = useMemo<ChartConfig>(
+		() =>
+			connections.reduce((acc, connection) => {
+				const newValue = {
+					...acc,
+					[connection.plaid_institution_id]: {
+						label: connection.name,
+						color: connection.primary_color,
+					},
+				};
+				return newValue;
+			}, {}),
+		[connections],
+	);
+
+	const chartData = useMemo(
+		() =>
+			connections.reduce(
+				(acc, connection) => {
+					const newValue = [
+						...acc,
+						{
+							bank: connection.plaid_institution_id,
+							amount: connection.accounts.reduce(
+								(acc, account) => acc + Number(account.available_balance),
+								0,
+							),
+							fill: connection.primary_color as string,
+						},
+					];
+					return newValue;
+				},
+				[] as { bank: string; amount: number; fill: string }[],
+			),
+		[connections],
+	);
+
 	return (
 		<Card>
-			<CardContent className="flex items-center justify-between gap-2 p-5">
+			<CardContent className="flex items-center justify-between gap-2 p-1 pl-5">
 				<div className="flex flex-col gap-1">
 					<p className="text-xs text-muted-foreground">Total balance</p>
 					<p className="text-3xl font-bold">{formatCurrency(totalBalance)}</p>
 				</div>
 
-				<div className="h-full w-full max-w-28">
-					<ChartContainer config={chartConfig}>
-						<LineChart
-							accessibilityLayer
-							data={[{ amount: 0 }, { amount: totalBalance }]}
-							margin={{
-								left: 12,
-								right: 12,
-							}}
+				<ChartContainer
+					config={chartConfig}
+					className="min-h-[100px] max-w-[100px]"
+				>
+					<PieChart>
+						<ChartTooltip
+							content={<ChartTooltipContent className="min-w-52" hideLabel />}
+						/>
+						<Pie
+							data={chartData}
+							dataKey="amount"
+							nameKey="bank"
+							innerRadius={29}
+							strokeWidth={5}
 						>
-							<Line
-								dataKey="amount"
-								type="natural"
-								stroke="var(--color-amount)"
-								strokeWidth={2}
-								dot={false}
+							<Label
+								content={({ viewBox }) => {
+									if (viewBox && 'cx' in viewBox && 'cy' in viewBox) {
+										return (
+											<text
+												x={viewBox.cx}
+												y={viewBox.cy}
+												textAnchor="middle"
+												dominantBaseline="middle"
+											>
+												<tspan
+													x={viewBox.cx}
+													y={viewBox.cy}
+													className="fill-foreground text-base font-bold"
+												>
+													{totalBalance}
+												</tspan>
+											</text>
+										);
+									}
+								}}
 							/>
-						</LineChart>
-					</ChartContainer>
-				</div>
+						</Pie>
+					</PieChart>
+				</ChartContainer>
 			</CardContent>
 		</Card>
 	);
