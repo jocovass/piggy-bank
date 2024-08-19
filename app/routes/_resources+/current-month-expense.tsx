72ad19dbdd5da22fea9851af741d316b4ebb4b75
Jozsef <joco.udv@gmail.com>
2024-08-19 21:43:37 +0100

    Remove console.log

--- app/routes/_resources+/current-month-expense.tsx
+++ app/routes/_resources+/current-month-expense.tsx
@@ -74,7 +74,7 @@ const chartConfig = {
 
 export default function CurrentMonthExpense() {
 	const lastMonthExpense = useFetcher<typeof loader>();
-	console.log(lastMonthExpense.data);
+
 	useEffect(() => {
 		lastMonthExpense.submit(null, {
 			method: 'GET',
